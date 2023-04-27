ea6bd6ef43f0cb81e5d41dc0dc98e4f977b42f39
Emily Iannizzi <eiannizzi18@gmail.com>
2023-04-26 21:29:20 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+document.querySelector("#main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+const name = "John"; 
+newHeader.textContent = `${name} is the champion`;
+
+document.body.appendChild(newHeader);
